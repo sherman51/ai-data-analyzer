cd16e3b8e8e5ea60ab193ff1e4594a1ded3e7e06
sherman51 <92359221+sherman51@users.noreply.github.com>
2025-08-18 17:36:17 +0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("BMI Calculator")
+
+st.sidebar.title("Enter your details:")
+height = st.sidebar.number_input("Height (in m)", min_value = 0.5, max_value = 2.5, step = 0.01)
+weight = st.sidebar.number_input("Weight (in kg)", min_value = 10.0, max_value = 300.0, step = 0.1)
