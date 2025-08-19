a01040ff3c181ee7c141e41df06f5b0c2ff528e5
sherman51 <92359221+sherman51@users.noreply.github.com>
2025-08-19 13:10:44 +0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -8,10 +8,11 @@ weight = st.sidebar.number_input("Weight (in kg)", min_value = 10.0, max_value =
 
 
 # Calculate BMI
-if st.sidebar.button("Calculate BMI")
+if st.sidebar.button("Calculate BMI"):
 bmi = weight/(height**2)
 
 st.write("Your BMI is: **{bmi:.2f}**")
+
 
 
 
